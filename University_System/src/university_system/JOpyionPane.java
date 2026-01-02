d28d6be49fe8960a0d70a6d114e1a45cae3817dc
sherinshaban <sherenshaban3308@gmail.com>
2026-01-02 08:59:22 +0200

    First upload for University System

---
+++ University_System/src/university_system/JOpyionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package university_system;
+
+/**
+ *
+ * @author DELL
+ */
+class JOpyionPane {
+
+    static void showMessageDialog(Object object, String data_has_been_stored) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ University_System/src/university_system/University_System.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package university_system;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author DELL
+ */
+public class University_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame fram = new LoginPage();
+        fram.setVisible(true);
+        fram.setLocation(100, 100);
+        fram.setResizable(false);
+    }
+    
+}
